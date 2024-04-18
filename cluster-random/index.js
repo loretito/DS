7f70b936b26f5cbb564821106df7d5d873136a7a
Cesar Munoz <cesar.munoz_r@mail.udp.cl>
2024-04-18 19:07:54 -0400

    Test

---
+++ cluster-random/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const path = require('path')
+const buildRedisClient = require('./service/redisClient')
+// Aqui van las otras dependencias como express y esas
+
+const redis = buildRedisClient()
+
+// Aqui va el resto del código
+
+//example of end point
+
+app.post('/save-data', async (request, response) => {
+    const { key, value } = request.body
+    await redis.set(key, value)
+    return response.status(201).render('home/index', {
+      layout: 'default',
+      dataSaved: true,
+    })
+  })
+
