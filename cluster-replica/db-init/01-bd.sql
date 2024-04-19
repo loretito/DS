6cfaf1d9bdb01d8ba647a614cdf8de1d0729fdfc
Cesar Munoz <cesar.munoz_r@mail.udp.cl>
2024-04-18 22:04:05 -0400

    hola lore emojis

---
+++ cluster-replica/db-init/01-bd.sql
@@ -0,0 +1,20 @@
+--asignatura|nombre|seccion|evento|horario|profesor|sede|paquete
+--CIG1002|INGLÉS GENERAL II|Sección 1|VACANTES CARRERA INGLÉS 01|LU MI 08:30 - 09:50|PALOMINOS MARCELA CATALINA|S-SANTIAGO|CIG1002_VC01
+--CIG1002|INGLÉS GENERAL II|Sección 10|VACANTES CARRERA INGLES 10|MI VI 14:30 - 15:50|MOYANO NATALY DEL SOL|S-SANTIAGO|CIG1002_VC10
+--CIG1002|INGLÉS GENERAL II|Sección 11|VACANTES CARRERA INGLES 11|LU MI 08:30 - 09:50|DINAMARCA OLGA MARGARITA|S-SANTIAGO|CIG1002_VC11
+
+
+CREATE TABLE IF NOT EXISTS asignatura (
+    bd_id SERIAL PRIMARY KEY,
+    codigo_asignatura VARCHAR(15),
+    nombre_asignatura VARCHAR(255),
+    seccion VARCHAR(15),
+    evento VARCHAR(255),
+    horario VARCHAR(255),
+    profesor VARCHAR(255),
+    sede VARCHAR(255),
+    paquete VARCHAR(255)
+);
+
+COPY asignatura(codigo_asignatura, nombre_asignatura, seccion, evento, horario, profesor, sede, paquete) 
+FROM '/docker-entrypoint-initdb.d/02-ramos.txt' DELIMITER '|' CSV HEADER;
---
+++ cluster-replica/service/redisClient.js
@@ -0,0 +1,44 @@
+const Redis = require('ioredis')
+
+/**
+ * Get an existing Redis client instance. Build one if necessary
+ * @return {Redis|null} redis client
+ * */
+function buildRedisClient() {
+  
+  try {
+    // Master host and port should be retrieved from environment variables or configuration
+    const masterHost = process.env.REDIS_MASTER_HOST;
+    const masterPort = process.env.REDIS_MASTER_PORT;
+
+    const client = new Redis({
+      host: masterHost,
+      port: masterPort,
+      enableAutoPipelining: true, // Optional: Enable auto-pipelining for performance
+    })
+
+    client.on('error', error => {
+      console.error('Redis Error', error)
+    })
+
+    client.on('connect', () => {
+      console.log('Redis Connection stablished')
+    })
+
+    client.on('ready', () => {
+      console.log('Redis client ready')
+    })
+
+    client.on('end', () => {
+      console.log('Redis client connection ended')
+    })
+
+    return client
+  } catch (error) {
+    console.error('Could not create a Redis client', error)
+
+    return null
+  }
+}
+
+module.exports = buildRedisClient
